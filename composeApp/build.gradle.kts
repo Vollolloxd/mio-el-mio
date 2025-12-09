1c4d6f041ee2e90e33706b323c80c2b438ec16a7
Vollollo <cristobal.villalobos8@gmail.com>
2025-12-09 19:26:17 -0300

    chore: agregar dependencia androidx.navigation:navigation-compose

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -29,6 +29,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+ implementation("androidx.navigation:navigation-compose:2.7.5")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
