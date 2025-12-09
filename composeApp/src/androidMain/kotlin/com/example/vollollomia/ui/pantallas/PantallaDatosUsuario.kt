15df35cb201d97e9ff6f96c699909b464cb3b132
Vollollo <cristobal.villalobos8@gmail.com>
2025-12-09 19:38:29 -0300

    fix: eliminar import no utilizado PerfilUsuario

--- composeApp/src/androidMain/kotlin/com/example/vollollomia/ui/pantallas/PantallaDatosUsuario.kt
+++ composeApp/src/androidMain/kotlin/com/example/vollollomia/ui/pantallas/PantallaDatosUsuario.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.vollollomia.models.dominio.nutricion.ObjetivoFisico
-import com.example.vollollomia.models.dominio.nutricion.PerfilUsuario
 import com.example.vollollomia.ui.tema.Colores
 
 @Composable
